635ee64fd8d5b29b91fc06f3d33f84ebcfb11916
JUNY0110 <98405970+JUNY0110@users.noreply.github.com>
2022-06-21 23:47:12 +0900

    Add: 1008번
    나눗셈 문제.

---
+++ BaekJoon/1008.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by 지준용 on 2022/06/21.
+//
+
+import Foundation
+
+let division = readLine()!.split(separator: " ").map{Double($0)!}
+print(division[0]/division[1])
+
+let division = readLine()!.split(separator: " ").map{Int($0)!}
+print(Double(division[0])/Double(division[1]))
