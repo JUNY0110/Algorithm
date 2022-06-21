a2bf510b0a46f93eb4c799931d1ebd1b381d862a
JUNY0110 <98405970+JUNY0110@users.noreply.github.com>
2022-06-21 16:38:52 +0900

    Add:10172번
    Dog 구현
    (하나의 변수로 여러 줄 구현)

---
+++ New Group/10172.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by 지준용 on 2022/06/21.
+//
+
+import Foundation
+
+let dog = #"""
+|\_/|
+|q p|   /}
+( 0 )"""\
+|"^"`    |
+||_/=\\__|
+"""#
+
+print(dog)
