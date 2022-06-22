6b120f2f583ed2fc88fd4422e59b9e5154a4ec49
JUNY0110 <98405970+JUNY0110@users.noreply.github.com>
2022-06-23 01:34:41 +0900

    Add: 25083번
    ', ", `, \ 등의 문자에 주의하며 새싹을 출력하는 문제

--- Algorithm/main.swift
+++ Algorithm/main.swift
@@ -2,7 +2,7 @@
 //  main.swift
 //  Algorithm
 //
-//  Created by 지준용 on 2022/06/22.
+//  Created by 지준용 on 2022/06/23.
 //
 
 import Foundation
---
+++ BaekJoon/25083.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by 지준용 on 2022/06/22.
+//
+
+import Foundation
+
+let leaf = #"""
+         ,r'"7
+r`-_   ,'  ,/
+ \. ". L_r'
+   `~\/
+      |
+      |
+"""#
+
+print(leaf)
