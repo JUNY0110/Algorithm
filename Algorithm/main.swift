d6fb54dfd92a7aeb848eb950e979f431b434d2a7
JUNY0110 <98405970+JUNY0110@users.noreply.github.com>
2022-06-27 00:11:22 +0900

    Add: 2741번
    1부터 N까지 출력하는 문제

--- Algorithm/main.swift
+++ Algorithm/main.swift
@@ -2,7 +2,7 @@
 //  main.swift
 //  Algorithm
 //
-//  Created by 지준용 on 2022/06/26.
+//  Created by 지준용 on 2022/06/27.
 //
 
 import Foundation
---
+++ Algorithm/반복문/2741.swift
@@ -0,0 +1,25 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by 지준용 on 2022/06/26.
+//
+
+import Foundation
+
+//let input = Int(String(readLine()!))!
+var input = Int(String(readLine()!))!
+
+for i in 1...input {
+    print(i)
+}
+//
+//while 1...input {
+//    num += 1
+//    print(num)
+//}
+var num = 0
+while num < input {
+    num += 1
+    print(num)
+}
