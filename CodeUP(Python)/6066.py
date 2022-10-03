11f5ff1796b85305ff4ed7c4376772e88b29a618
Juny0110 <wnsdyds403@naver.com>
2022-10-04 01:14:20 +0900

    6066 : [기초-조건/선택실행구조] 정수 3개 입력받아 짝/홀 출력하기

---
+++ CodeUP(Python)/6066.py
@@ -0,0 +1,20 @@
+# 6066
+
+a, b, c = map(int, input().split())
+o = "odd"
+e = "even"
+
+if a % 2 == 0:
+    print(e)
+else:
+    print(o)
+
+if b % 2 == 0:
+    print(e)
+else:
+    print(o)
+
+if c % 2 == 0:
+    print(e)
+else:
+    print(o)
