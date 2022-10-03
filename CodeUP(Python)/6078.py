c401977b25c7102e49f9fbe86c3e02e2d9a26ed9
Juny0110 <wnsdyds403@naver.com>
2022-10-04 01:16:13 +0900

    6078 : [기초-종합] 원하는 문자가 입력될 때까지 반복 출력하기

---
+++ CodeUP(Python)/6078.py
@@ -0,0 +1,17 @@
+# 6078
+
+c = 1
+a = ord('q')
+
+while True:
+    if c == a:
+        break
+    c = ord(input())
+    print(chr(c))
+
+while True:
+    x = input()
+    print(x)
+
+    if x == 'q':
+        break
