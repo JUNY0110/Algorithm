a5a108f2891401c8e4857319bfaac0876450b6b1
Juny0110 <wnsdyds403@naver.com>
2023-01-05 22:27:11 +0900

    Add: (2292) Beehive
    - 벌집이 형성되는 규칙에 따라 벌집의 위치를 구하는 문제

---
+++ Algorithm/기본수학1/2292.swift
@@ -0,0 +1,30 @@
+//
+//  2292.swift
+//  Algorithm
+//
+//  Created by 지준용 on 2023/01/05.
+//
+
+import Foundation
+
+class BaekJoon2292 {
+    func run() {
+        solution1()
+    }
+    
+    func solution1() {
+        if let input = readLine(),
+           let num = Int(input) {
+            var n = 0
+            
+            if num == 1 {
+                n = 1
+            } else {
+                while num > 1 + 6 * ((n - 1) * n / 2) {
+                    n += 1
+                }
+            }
+            print(n)
+        }
+    }
+}
