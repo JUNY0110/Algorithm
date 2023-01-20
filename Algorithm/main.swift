26266d0b3aa4bb74ce802b86a11747e8dddb7bd3
Juny0110 <wnsdyds403@naver.com>
2023-01-20 14:33:19 +0900

    Style: modify initialize style

--- Algorithm/main.swift
+++ Algorithm/main.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+let main = BaekJoon2563()
+
+//let startTime = CFAbsoluteTimeGetCurrent()
+//var n = Int(String(readLine()!))!
+
+//for _ in stride(from: n, to: 0, by: -1) {
+    main.run()
+//}
+//print(CFAbsoluteTimeGetCurrent() - startTime)
