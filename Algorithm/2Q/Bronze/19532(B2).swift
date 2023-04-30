b068676e76cf18dcc6c3b1f48d5fe1b5e2a2bb33
Juny0110 <wnsdyds403@naver.com>
2023-05-01 01:12:16 +0900

    Add: (19532) Solution of the system of equations
    - 연립방정식 x, y의 해 구하기
    - 풀이: 대입법, 가감법

---
+++ Algorithm/2Q/Bronze/19532(B2).swift
@@ -0,0 +1,47 @@
+//
+//  19532.swift
+//  Algorithm
+//
+//  Created by 지준용 on 2023/04/30.
+//
+
+import Foundation
+
+class BaekJoon19532 {
+    func run() {
+        solution1()
+    }
+    
+    /// - NOTE: 문제풀이. 연립방정식의 x, y 의 해 구하기
+    /// - IMPORTANT: 대입법, 가감법
+    
+    /*
+     ax + by = c
+     dx + ey = f
+     */
+    
+    func solution1() {
+        let nums = readLine()!.split(separator: " ").map { Int(String($0))! }
+        let a = nums[0], b = nums[1], c = nums[2], d = nums[3], e = nums[4], f = nums[5]
+        let scope = Array<Int>(-999...999)
+        
+        for x in scope {
+            for y in scope {
+                if (a * x) + (b * y) == c && (d * x) + (e * y) == f {
+                    print(x, y)
+                    break
+                }
+            }
+        }
+    }
+    
+    func solution2() {
+        let nums = readLine()!.split(separator: " ").map { Int(String($0))! }
+        let a = nums[0], b = nums[1], c = nums[2], d = nums[3], e = nums[4], f = nums[5]
+        
+        let x = (c*e - b*f) / (a*e - b*d)
+        let y = (c*d - a*f) / (b*d - a*e)
+        
+        print(x, y)
+    }
+}
--- Algorithm/main.swift
+++ Algorithm/main.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-let main = BaekJoon14215()
+let main = BaekJoon19532()
 
 //let startTime = CFAbsoluteTimeGetCurrent()
 //var n = Int(String(readLine()!))!
