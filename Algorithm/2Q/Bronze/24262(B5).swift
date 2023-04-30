4daad9f0ef9ef1d76c0f45734c55e9649cf64f50
Juny0110 <wnsdyds403@naver.com>
2023-04-30 23:32:30 +0900

    Add: (24262) Time Complexity1

---
+++ Algorithm/2Q/Bronze/24262(B5).swift
@@ -0,0 +1,26 @@
+//
+//  24262.swift
+//  Algorithm
+//
+//  Created by 지준용 on 2023/04/30.
+//
+
+import Foundation
+
+class BaekJoon24262 {
+    func run() {
+        solution()
+    }
+    
+    /// - NOTE: 문제풀이. 알고리즘 수행시간
+    /// - IMPORTANT: 예제의 수행 횟수, 최고차항의 차수 출력
+    /*
+    MenOfPassion(A[], n) {
+    i = ⌊n / 2⌋;
+    return A[i]; # 코드1
+     */
+    
+    func solution() {
+        print(1, 0, separator: "\n")
+    }
+}
