05efe2de0e5c6898e789da998a6d5afe5426aabd
Juny0110 <wnsdyds403@naver.com>
2023-05-21 19:21:22 +0900

    Add: (24723) Green tower

---
+++ Algorithm/2Q/Bronze/24723(B4).swift
@@ -0,0 +1,20 @@
+//
+//  24723.swift
+//  Algorithm
+//
+//  Created by 지준용 on 2023/05/21.
+//
+
+import Foundation
+
+class BaekJoon24723 {
+    func run() {
+        solution()
+    }
+    
+    /// - NOTE: 문제풀이. 녹색거탑
+    
+    func solution() {
+        print(pow(2, Int(readLine()!)!))
+    }
+}
--- Algorithm/main.swift
+++ Algorithm/main.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-let main = BaekJoon1463()
+let main = BaekJoon24723()
 
 //let startTime = CFAbsoluteTimeGetCurrent()
 //var n = Int(String(readLine()!))!
