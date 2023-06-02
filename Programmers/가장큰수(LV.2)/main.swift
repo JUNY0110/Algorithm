dca36176e69a9d6ba1ac1309830005c2980694b8
Juny0110 <wnsdyds403@naver.com>
2023-06-02 11:22:16 +0900

    Add: (LV.2) Largest number

---
+++ Programmers/가장큰수(LV.2)/main.swift
@@ -0,0 +1,41 @@
+//
+//  main.swift
+//  Programmers
+//
+//  Created by 지준용 on 2023/06/02.
+//
+
+import Foundation
+
+import Foundation
+let main = Main()
+
+main.run()
+
+class Main {
+    func run() {
+        let numbers = readLine()!.split(separator: " ").compactMap { Int($0) }
+        print(solution(numbers))
+    }
+    
+    // 6 10 2 -> "6210"
+    // 3 30 34 5 9 -> "9534330"
+    
+    func solution(_ numbers:[Int]) -> String {
+        // 문자열 정렬
+        // 두 수를 이어붙인 값이 큰 순서대로 정렬
+        // 만약 모든 수가 0이라면 0 출력
+        // 그렇지 않으면 수 출력
+        var numbers = numbers.map { String($0) }
+        
+        numbers.sort(by: {
+            return $0+$1 > $1+$0
+        })
+        
+        if numbers[0] == "0" {
+            return "0"
+        } else {
+            return numbers.joined()
+        }
+    }
+}
