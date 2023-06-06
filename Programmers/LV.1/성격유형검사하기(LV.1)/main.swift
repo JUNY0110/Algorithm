96b1abf211fb32208dac4272771b6129b6106b16
Juny0110 <wnsdyds403@naver.com>
2023-06-06 18:43:37 +0900

    Update: (LV.1) Personality type test

--- Programmers/LV.1/성격유형검사하기(LV.1)/main.swift
+++ Programmers/LV.1/성격유형검사하기(LV.1)/main.swift
@@ -51,4 +51,49 @@ class Main {
         
         return result
     }
+
+    // MARK: - //////////////////////////////////////////////////////////////////
+    struct PersonalityTest {
+        var choices: [Int]
+        var survey: [String]
+        
+        init(choices: [Int], survey: [String]) {
+            self.choices = choices
+            self.survey = survey
+        }
+        
+        func calculateScore(of choices:[Int], at survey:[String]) -> [Character: Int] {
+            var indicators: [Character: Int] = ["R": 0, "T": 0, "C": 0, "F": 0,
+                                                "A": 0, "N": 0, "J": 0, "M": 0]
+            
+            for (question, choice) in zip(survey, choices) {
+                switch choice {
+                case 1..<4: indicators[question.first!]! += (4 - choice) // 첫번째 지표
+                case 5...7: indicators[question.last!]! += (choice - 4)  // 두번째 지표
+                default: break
+                }
+            }
+
+            return indicators
+        }
+        
+        func surveyResult(of indicatorScores: [Character: Int]) -> String {
+            let indicatorTypes: [(Character, Character)] = [("R", "T"), ("C", "F"),
+                                                            ("J", "M"), ("A", "N")]
+            var result = ""
+            
+            for t in indicatorTypes {
+                result += String((indicatorScores[t.0]! >= indicatorScores[t.1]!) ? t.0 : t.1) // 지표 점수 계산
+            }
+            
+            return result
+        }
+    }
+    
+    func solution2(_ survey:[String], _ choices:[Int]) -> String {
+        let personalityTest = PersonalityTest(choices: choices, survey: survey)
+        let testerScore = personalityTest.calculateScore(of: choices, at: survey)
+        
+        return personalityTest.surveyResult(of: testerScore)
+    }
 }
